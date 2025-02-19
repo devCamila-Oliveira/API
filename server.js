cf0e89da56fa2860d193d4e40f4840eda42a587b
Camila <devcamilasouza@gmail.com>
2025-02-19 02:14:29 -0300

    Adicionando configs de banco

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const app = express()
-
-app.get('/teste', (req, res) => {
-    res.send('Ok, deu bom :)')
-})
-
-app.listen(3000)
---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users)
+})
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name, 
+            age: req.body.age
+        }
+    })
+
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})
+
+app.listen(3000)
